ecc8c44f209ffc68d7854e776c63a07466c1cc24
Muna Hussen <munahussen@Munas-MBP.lan>
2021-07-25 17:58:53 -0400

    Made greeting section responsive

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 const app = {};
 
+app.animations = () => {
+    gsap.from(".name__letter", {
+        x: -50,
+        duration: 6,
+        stagger: {
+            from: "left",
+            amount: 1.5
+          }
+    });
+}
+
 app.toggleNav = () => {
     $('.nav-icons').click(() => {
         $('.nav-list').toggleClass('hidden');
+        $('body').toggleClass('lock');
+        $('.featured-work-circle').toggleClass('hidden');
+        $('.scroll').toggleClass('hidden');
     })
 
     $('.nav-links').click(() => {
         $('.nav-list').toggleClass('hidden');
+        $('body').toggleClass('lock');
+        $('.featured-work-circle').toggleClass('hidden');
+        $('.scroll').toggleClass('hidden');
     })
 }
 
 app.init = () => {
     app.toggleNav();
+    app.animations();
 }
 
 $(document).ready(() => {
